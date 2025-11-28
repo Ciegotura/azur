3b22b48bc64fc6d24f2f22a3eb47bed1adc420e3
Ciegotura <81392607+Ciegotura@users.noreply.github.com>
2025-11-28 18:11:22 +0100

    init

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import os
+import pyodbc
+
+app = Flask(__name__)
+
+def get_db_connection2():
+    conn_str = "DRIVER={ODBC Driver 18 for SQL Server};Server=tcp:owch-sql-server-pc38.database.windows.net,1433;Initial Catalog=flask-db;Persist Security Info=False;UID=sqladmin;Password=Felgi123lol1000;MultipleActiveResultSets=False;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    conn = pyodbc.connect(conn_str)
+    return conn
+
+def get_db_connection():
+    conn_str = os.environ.get('SQLAZURECONNSTR_DB_CONNECTION_STRING')
+    conn = pyodbc.connect(conn_str)
+    return conn
+
+
+@app.route('/')
+def index():
+    tasks_html = "<h2>Lista zadan z bazy danych:</h2><ul>"
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT Title FROM Tasks")
+        rows = cursor.fetchall()
+        for row in rows:
+            tasks_html += f"<li>{row.Title}</li>"
+        conn.close()
+    except Exception as e:
+        tasks_html += f"<li>Blad polaczenia z baza: {str(e)}</li>"
+
+    tasks_html += "</ul>"
+    return tasks_html
